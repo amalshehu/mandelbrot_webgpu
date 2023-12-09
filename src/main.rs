53151468200955d7d7474adedeb853aa62981b9d
amalshehu <amalshehu@gmail.com>
2023-12-09 20:35:02 +0530

    Create mandelbrot

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use env_logger;
-use wgpu::util::DeviceExt;
+use wgpu::{Backends, Instance, PowerPreference, RequestAdapterOptions, SurfaceConfiguration};
 use winit::{
     event::*,
     event_loop::{ControlFlow, EventLoop},
@@ -31,25 +31,29 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
         .unwrap();
 
     // Configure the surface to use the correct format
-    let config = SurfaceConfiguration {
+    let mut config = SurfaceConfiguration {
         usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-        format: surface.get_supported_formats(&adapter)[0],
+        format: wgpu::TextureFormat::Rgba16Float, // Replace with a valid format [Bgra8Unorm, Bgra8UnormSrgb, Rgba16Float]
         width: size.width,
         height: size.height,
         present_mode: wgpu::PresentMode::Fifo,
     };
     surface.configure(&device, &config);
 
+    // Load the shader
     let shader = device.create_shader_module(&wgpu::ShaderModuleDescriptor {
         label: Some("Shader"),
         source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
     });
+
+    // Create render pipeline layout
     let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
         label: Some("Render Pipeline Layout"),
         bind_group_layouts: &[],
         push_constant_ranges: &[],
     });
 
+    // Create render pipeline
     let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
         label: Some("Render Pipeline"),
         layout: Some(&render_pipeline_layout),
@@ -71,6 +75,69 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
         depth_stencil: None,
         multisample: wgpu::MultisampleState::default(),
     });
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::RedrawRequested(_) => {
+                let output = match surface.get_current_frame() {
+                    Ok(output) => output,
+                    Err(wgpu::SurfaceError::Outdated) => {
+                        surface.configure(&device, &config);
+                        surface.get_current_frame().unwrap()
+                    }
+                    Err(_) => return,
+                };
+                let view = output
+                    .output
+                    .texture
+                    .create_view(&wgpu::TextureViewDescriptor::default());
+                let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                    label: Some("Render Encoder"),
+                });
+
+                {
+                    let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                        label: Some("Render Pass"),
+                        color_attachments: &[wgpu::RenderPassColorAttachment {
+                            view: &view,
+                            resolve_target: None,
+                            ops: wgpu::Operations {
+                                load: wgpu::LoadOp::Clear(wgpu::Color {
+                                    r: 0.1,
+                                    g: 0.2,
+                                    b: 0.3,
+                                    a: 1.0,
+                                }),
+                                store: true,
+                            },
+                        }],
+                        depth_stencil_attachment: None,
+                    });
+
+                    render_pass.set_pipeline(&render_pipeline);
+                    render_pass.draw(0..6, 0..1);
+                }
+
+                queue.submit(Some(encoder.finish()));
+            }
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => *control_flow = ControlFlow::Exit,
+            Event::WindowEvent {
+                event: WindowEvent::Resized(size),
+                ..
+            } => {
+                config.width = size.width;
+                config.height = size.height;
+                surface.configure(&device, &config);
+                window.request_redraw(); // Request redraw on resize
+            }
+            _ => {}
+        }
+    });
 }
 
 fn main() {
@@ -78,16 +145,5 @@ fn main() {
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
 
-    event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Wait;
-
-        match event {
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                ..
-            } => *control_flow = ControlFlow::Exit,
-            _ => {}
-        }
-    });
-    pollster::block_on(run(event_loop, window))
+    pollster::block_on(run(event_loop, window));
 }
